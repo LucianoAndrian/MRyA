2aab187bfffca8c1b1b8df825c6dd6e0bab53364
AURI <lucianoandrian@hotmail.com>
2021-04-14 01:13:08 -0300

    p3

--- funciones.R
+++ funciones.R
@@ -37,8 +37,10 @@ recta.reg = function(x, y, residuo = FALSE, plot = FALSE, Summary = FALSE){
 
 biserial.test = function(tabla, intervalos, alpha){
   
+  # intervalos debe ser el valor medio de los intervalos. ingresar un vector con esos valores
+  
   if(!is.data.frame(tabla) & !is.matrix(tabla)){
-    stop("tabla debe ser un data.frame")
+    stop("tabla debe ser un data.frame o una matriz")
   }
   
   if(length(tabla[,1]) != length(intervalos)){
@@ -87,13 +89,136 @@ biserial.test = function(tabla, intervalos, alpha){
   
   # test
   if(abs(est) > abs(zc)){
-    print(paste("Rechazo H0 con un ", (1-alpha)*100, "% de confianza", sep = ""))
+    print(paste("Rechazo H0 con un ", (1-alpha)*100, "% de confianza", sep = ""), quote = F)
   } else {
-    print(paste("NO rechazo H0 con un ", (1-alpha)*100, "% de confianza", sep = ""))
+    print(paste("NO rechazo H0 con un ", (1-alpha)*100, "% de confianza", sep = ""), quote = F)
   }
   
   result = c(rb, sigmab, est)
   names(result) = c("Rb", "Sigma b", "Estadistico")
+  
+  return(result)
+  
+}
+
+
+#### tetracor_test ####
+tetracor_test = function(tabla, alpha){
+  
+  if(!is.data.frame(tabla) & !is.matrix(tabla)){
+    stop("tabla debe ser un data.frame o una matriz")
+  }
+  
+  if(length(tabla[,1]) != 2 & length(tabla[1,]) != 2){
+    stop("la tabla debe ser de 2x2")
+  }
+  
+  if(is.character(alpha) | alpha > 1){
+    stop("ALPHA debe estar entre 0 y 1")
+  }
+  
+  
+  aux = (sqrt(tabla[1,1]*tabla[2,2]) - sqrt(tabla[1,2]*tabla[2,1]))/sqrt(tabla[1,1]*tabla[2,2]) + sqrt(tabla[1,2]*tabla[2,1])
+  
+  # Rt
+  rt = sin((pi/2)*aux)
+  
+  
+  # zh y zk
+  
+  n = sum(tabla)
+  
+  ph = sum(tabla[1,])/n
+  pk = sum(tabla[2,])/n
+  
+  
+  # antes era con q o con p daba lo mismo, pero lo hago con q. (1-p)
+  zh = dnorm(qnorm(1-ph))
+  zk = dnorm(qnorm(1-pk))
+  
+  # Se
+  
+  aux = sum(tabla[1,])*sum(tabla[,1])*sum(tabla[,2])*sum(tabla[2,])
+  
+  se = (1/(zk*zh*sqrt(n)))*sqrt(aux/n**4)
+  
+  est = rt/se
+  
+  # Z critico
+  zc = qnorm(p = 1-alpha/2)
+  
+  ## zc = qt(p = 1-alpha/2, df = 1) ????
+  
+  
+  # test
+  if(abs(est) > abs(zc)){
+    print(paste("Rechazo H0 con un ", (1-alpha)*100, "% de confianza", sep = ""), quote = F)
+  } else {
+    print(paste("NO rechazo H0 con un ", (1-alpha)*100, "% de confianza", sep = ""), quote = F)
+  }
+  
+  result = c(rt, se, est)
+  names(result) = c("Rt", "Sigma e", "Estadistico")
+  
+  return(result)
+  
+  
+}
+
+
+#### tabla de contingencia ####
+
+ctg_test = function(tabla, alpha, totales = FALSE){
+  
+  if(!is.data.frame(tabla) & !is.matrix(tabla)){
+    stop("tabla debe ser un data.frame o una matriz")
+  }
+  
+  if(is.character(alpha) | alpha > 1){
+    stop("ALPHA debe estar entre 0 y 1")
+  }
+  
+  
+  if(totales){
+    tabla = tabla[1:(nrow(tabla)-1), 1:(ncol(tabla)-1)]
+  } 
+  
+  tot_col = apply(tabla, c(2), sum)
+  tot_row = apply(tabla, c(1), sum)
+  total = sum(tot_row)
+  
+  #tabla random
+  tabla_rand = matrix(nrow = nrow(tabla), ncol = ncol(tabla))
+  
+  for(r in 1:nrow(tabla)){
+    
+    tabla_rand[r,] = tot_row[r]*tot_col/total
+   
+  }
+  print(tabla_rand)
+  
+  
+  # estadistico chi-sq
+  est = sum(((tabla - tabla_rand)**2)/tabla_rand)
+  
+  # chi-teo
+  est_teo = qchisq(p = (1-alpha), df = (nrow(tabla) - 1)*(ncol(tabla) - 1))
+  
+  
+  # coeficiente de contingencia 
+  
+   c = sqrt(est/(est+total)) 
+  
+  # test
+  if(abs(est) > abs(est_teo)){
+    print(paste("Rechazo H0 con un ", (1-alpha)*100, "% de confianza", sep = ""), quote = F)
+  } else {
+    print(paste("NO rechazo H0 con un ", (1-alpha)*100, "% de confianza", sep = ""), quote = F)
+  }
+  
+  
+  result = c(est, est_teo, c)
+  names(result) = c("Chi.c", "Chi.teo", "C")
   
   return(result)
   
---
+++ practica3.R
@@ -0,0 +1,129 @@
+# practica 2 
+ruta_datos = "/home/auri/Facultad/Doc/Materias/MRyA/Practicas/P3/"
+system(command = "mkdir P3/SalidasP3")
+ruta_salidas = "/home/auri/Facultad/Doc/Materias/MRyA/Practicas/P3/SalidasP3"
+
+source("funciones.R")
+
+## 1
+
+ll = c(2, 9, 9, 29, 41, 60, 57, 17, 10, 1)
+nll = c(0, 0, 0, 5, 5, 19, 41, 41, 13, 9)
+
+datos = data.frame(ll = ll, nll = nll)
+
+med = function(x,y){
+  res = (x + y)/2
+  return(res)
+}
+
+
+med_intervalo  = c(722.375, med(725,729.75), med(730,734.75), med(735,739.75),
+                   med(740,744.75), med(745,749.75), med(750,754.75),
+                   med(755,759.75), med(760,764.75), med(765,769.75))
+
+
+biserial.test(tabla = datos, intervalos = med_intervalo, alpha = 0.05)
+
+## 2
+
+niebla = c(2, 4, 5, 2, 1, 1, 0)
+n.niebla = c(1, 4, 5, 10, 8, 3, 3)
+
+datos = data.frame(niebla = niebla, n.niebla = n.niebla)
+
+med_intervalo = c(-2, 1, 4, 7, 10, 13, 16) 
+
+biserial.test(tabla = datos, intervalos = med_intervalo, alpha = 0.05)
+
+
+## 3 # chquear funcion
+
+x = c(685,376)
+y = c(158,2068)
+tabla.a = data.frame(si = x, no = y)
+
+x = c(572,283)
+y = c(600,1832)
+tabla.b = data.frame(si = x, no = y)
+
+x = c(834,283)
+y = c(338,1832)
+tabla.c = data.frame(si = x, no = y)
+
+#a
+tetracor_test(tabla = tabla.a, alpha = 0.05)
+
+#b
+tetracor_test(tabla = tabla.b, alpha = 0.05)
+
+#c
+tetracor_test(tabla = tabla.c, alpha = 0.05)
+
+
+#### 4 
+# maiz
+a = c(3,13,16,32)
+b = c(5,22,5,32)
+c = c(13,17,2,32)
+d = c(21,52,23,96)
+
+maiz = data.frame(a, b, c, d)
+
+ctg_test(tabla = maiz, alpha = 0.05, totales = T)
+
+
+#trigo
+
+a = c(7,16,9,32)
+b = c(5,20,9,31)
+c = c(9,15,8,32)
+d = c(21,51,23,95)
+
+trigo = data.frame(a, b, c, d)
+ctg_test(tabla = trigo, alpha = 0.05, totales = T)
+
+
+## 5 REVISAR
+library(readxl)
+datos = as.data.frame(read_xlsx(paste(ruta_datos, "/Datos_pp_ej5_P3.xlsx", sep = "")
+                                ,sheet = 1 ))
+
+
+
+# periodos en comun
+# mdp - cba, filas: 109:1020
+# ros - cba, filas: 13:1020
+# con - cb, filas: 570:1008
+
+datos[min(which(!is.na(datos[,5]))):max(which(!is.na(datos[,3]))),3]
+
+per = list(); per[[1]] = c(109:1020)
+per[[3]] = c(13:1020); per[[4]] = c(570:1008)
+
+tabla = data.frame(Estaciones = NA, Período = NA, Cant.datos = NA, chi.sq = NA, C = NA, significancia = NA)
+
+aux = matrix(data = NA, nrow = 5, ncol = 2)
+
+for(i in c(1,3,4)){
+  
+  for(q in 1:5){
+    
+    aux[q,1] = length(which(datos[per[[i]],4] == q)) #cordoba
+    aux[q,2] = length(which(datos[per[[i]],2+i] == q)) #
+    
+  }
+
+  ctg = ctg_test(tabla = aux, alpha = 0.05)
+  
+  tabla[i,2] = paste(min(per[[i]]), "-", max(per[[i]]))
+  tabla[i,3] = length(per[[i]])
+  tabla[[i,4]] = ctg[1]; tabla[[i,5]] = ctg[[3]]
+}
+
+
+
+
+
+
+
